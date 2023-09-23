94b6e05a06f2a461a19c1ec75023234e1cabb924
Emil Broman <emil@emilbroman.me>
2023-09-23 15:18:05 +0200

    Add Redis wrapper package

---
+++ packages/redis/RedisClientFactory.ts
@@ -0,0 +1,136 @@
+import {
+  Casing,
+  Codec,
+  JSONCodec,
+  ReadModelClientFactory,
+} from "@sequent/core";
+import { Redis, RedisKey } from "ioredis";
+
+export class RedisClientFactory
+  implements ReadModelClientFactory<RedisClient<any>>
+{
+  readonly #client: Redis;
+  readonly #codec: Codec<any>;
+  readonly #method: RedisNamespacingMethod;
+  readonly namingConvention = Casing.PascalCase;
+
+  constructor(
+    client: Redis,
+    opts: { codec?: Codec<any>; method?: RedisNamespacingMethod } = {},
+  ) {
+    this.#client = client;
+    this.#codec = opts.codec ?? new JSONCodec();
+    this.#method = opts.method ?? RedisNamespacingMethod.Prefix;
+  }
+
+  async make<TModel>(namespace: string): Promise<RedisClient<TModel>> {
+    switch (this.#method) {
+      case RedisNamespacingMethod.Prefix:
+        return new PrefixNamespacedRedisClient({
+          client: this.#client,
+          codec: this.#codec,
+          namespace,
+        });
+
+      case RedisNamespacingMethod.Hash:
+        return new HashNamespacedRedisClient({
+          client: this.#client,
+          codec: this.#codec,
+          namespace,
+        });
+    }
+  }
+}
+
+export interface RedisClient<TModel> {
+  get(key: RedisKey): Promise<TModel | undefined>;
+  set(key: RedisKey, value: TModel): Promise<void>;
+  exists(key: RedisKey): Promise<boolean>;
+}
+
+export enum RedisNamespacingMethod {
+  Prefix = "PREFIX",
+  Hash = "HASH",
+}
+
+export class PrefixNamespacedRedisClient<TModel>
+  implements RedisClient<TModel>
+{
+  readonly #client: Redis;
+  readonly #codec: Codec<TModel>;
+  readonly #namespace: string;
+
+  constructor(opts: {
+    client: Redis;
+    codec: Codec<TModel>;
+    namespace: string;
+  }) {
+    this.#client = opts.client;
+    this.#codec = opts.codec;
+    this.#namespace = opts.namespace;
+  }
+
+  #key(key: RedisKey): Buffer {
+    return Buffer.concat([
+      Buffer.from(this.#namespace + ":"),
+      Buffer.from(key),
+    ]);
+  }
+
+  async get(key: RedisKey): Promise<TModel | undefined> {
+    const value = await this.#client.getBuffer(this.#key(key));
+    if (value == null) {
+      return undefined;
+    }
+
+    return this.#codec.deserialize(value);
+  }
+
+  async exists(key: RedisKey): Promise<boolean> {
+    return (await this.#client.exists(this.#key(key))) === 1;
+  }
+
+  async set(key: RedisKey, value: TModel) {
+    await this.#client.set(
+      this.#key(key),
+      Buffer.from(this.#codec.serialize(value)),
+    );
+  }
+}
+
+export class HashNamespacedRedisClient<TModel> implements RedisClient<TModel> {
+  readonly #client: Redis;
+  readonly #codec: Codec<TModel>;
+  readonly #namespace: string;
+
+  constructor(opts: {
+    client: Redis;
+    codec: Codec<TModel>;
+    namespace: string;
+  }) {
+    this.#client = opts.client;
+    this.#codec = opts.codec;
+    this.#namespace = opts.namespace;
+  }
+
+  async get(key: RedisKey): Promise<TModel | undefined> {
+    const value = await this.#client.hgetBuffer(this.#namespace, key);
+    if (value == null) {
+      return undefined;
+    }
+
+    return this.#codec.deserialize(value);
+  }
+
+  async exists(key: RedisKey): Promise<boolean> {
+    return (await this.#client.hexists(this.#namespace, key)) === 1;
+  }
+
+  async set(key: RedisKey, value: TModel) {
+    await this.#client.hset(
+      this.#namespace,
+      key,
+      Buffer.from(this.#codec.serialize(value)),
+    );
+  }
+}
